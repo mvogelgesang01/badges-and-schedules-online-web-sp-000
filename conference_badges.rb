94a15c9716ab16b0249f5d501eeb0a1a457034c9
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-25 20:54:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(speaker)
+ return "Hello, my name is #{speaker}."
+end
+
+speakers_lineup = ["Melissa", "Nick", "Maddie", "Matt", "Paul", "Maryann", "Zoe"]
+
+def batch_badge_creator(speakers_lineup)
+ speakers_lineup.collect {|speaker| badge_maker(speaker)}
+end
